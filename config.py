80218445e863d8e030a4745f0b1658a8745d7ca9
Chen Katzir <chenka09@gmail.com>
2024-01-22 18:39:33 +0000

    added baseDir and the csv file

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class ProjConfig():
     def __init__(self):
         # Default configuration values
-        self.baseDir = '/content/drive/MyDrive/TransferLearningProject'
+        self.baseDir = './baseDir'
         self.baseDirPelvic = os.path.join(self.baseDir,'PelvicMRData/data')
         self.baseDirAmos22 = os.path.join(self.baseDir,'amos22/amos22')
 
